ee85ddac63d9fcb2d9830fb073d1ed08f851fe19
nexus161 <nexus.hedspi@gmail.com>
2016-10-08 20:39:13 +0000

    search

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,9 @@ class Review < ActiveRecord::Base
 	validates :image,  presence: true, length: { minimum: 1 }
 	validates :content,  presence: true, length: { minimum: 1 }
 	mount_uploader :image, ReviewImageUploader
+	
+	def self.search(search)
+	  # Title is for the above case, the OP incorrectly had 'name'
+	  where("title LIKE ?", "%#{search}%")
+	end
 end
