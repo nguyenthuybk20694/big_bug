bce1bd48a03b4fee0a2ee6c771d88b4788dd10d9
ducngocvnk57 <ducngocvnk57@gmail.com>
2016-10-09 07:45:01 +0000

    add like

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161008200906) do
-=======
 ActiveRecord::Schema.define(version: 20161009045134) do
->>>>>>> acts_as_votable
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "provider"
